136f5a1b5836d2c20067572ceb0145f719ab336f
Jonghyung <tofutree23@gmail.com>
2022-01-09 19:10:38 +0900

    add: day3

--- 1-basic.js
+++ 1-basic.js
@@ -22,3 +22,24 @@ const calcTriangle = (a, b, c) => {
 };
 
 console.log(calcTriangle(3, 4, 1));
+
+/** 3. How many pencils do you need to hand out to all students? */
+const calcDoseOfPencils = (n) => Math.ceil(n / 12);
+
+console.log(calcDoseOfPencils(25));
+
+/** 4.The sum from 1 to N */
+const getSum = (n) => {
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    answer += i;
+  }
+  return answer;
+};
+
+console.log(getSum(5));
+
+/** 5. Min value of Array */
+const getMinValue = (arr) => Math.min(...arr);
+
+console.log(getMinValue([5, 4, 2, 6, 3, 11, 1]));
