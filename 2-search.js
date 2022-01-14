54ccf62a9657e13385e00f273d48c4b0da2f764b
Jonghyung <tofutree23@gmail.com>
2022-01-14 21:59:07 +0900

    add: day8

--- 2-search.js
+++ 2-search.js
@@ -1,9 +1,48 @@
 /** 1. Receive N (1<=N<=100) integers and output only those larger than its immediate predecessor. */
 const findBig = (numArr) => {
   return numArr.reduce((prev, cur, idx) => {
-    if (idx === 0) prev.push(cur);
-    if (cur > numArr[idx - 1]) prev.push(cur);
+    if (idx === 0 || cur > numArr[idx - 1]) prev.push(cur);
     return prev;
   }, []);
 };
 console.log(findBig([7, 3, 9, 5, 6, 12]));
+
+/** 2. Who can I see? */
+const getPeopleCount = (heightArr) => {
+  return heightArr.reduce((prev, cur) => {
+    if (prev.length === 0 || prev[prev.length - 1] < cur) prev.push(cur);
+    return prev;
+  }, []).length;
+};
+console.log(getPeopleCount([130, 135, 148, 140, 145, 150, 150, 153]));
+
+/** 3. Rock, Scissors, Paper */
+const letsPlay = (candidatesA, candidatesB) => {
+  return candidatesA.reduce((prev, cur, idx) => {
+    if (cur === candidatesB[idx]) {
+      prev.push("Draw");
+      return prev;
+    }
+    switch (cur) {
+      case "rock":
+        if (candidatesB[idx] === "scissors") prev.push("A Win");
+        else prev.push("B Win");
+        break;
+      case "scissors":
+        if (candidatesB[idx] === "paper") prev.push("A Win");
+        else prev.push("B Win");
+        break;
+      case "paper":
+        if (candidatesB[idx] === "rock") prev.push("A Win");
+        else prev.push("B Win");
+        break;
+    }
+    return prev;
+  }, []);
+};
+console.log(
+  letsPlay(
+    ["rock", "paper", "scissors", "scissors", "paper"],
+    ["paper", "paper", "rock", "scissors", "rock"]
+  )
+);
