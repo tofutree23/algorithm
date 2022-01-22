c60a523bc2db7fce20337273f3236514dc80b4d7
Jonghyung <tofutree23@gmail.com>
2022-01-22 10:51:41 +0900

    add: day16 - temp

--- 4-bruteforce.js
+++ 4-bruteforce.js
@@ -23,3 +23,20 @@ const getMax = (numArr) => {
 console.log(getMax([128, 460, 603, 40, 521, 137, 123]));
 
 /** 2. Reversed Prime */
+const isPrime = (number) => {
+  if (number < 2) return false;
+
+  const sqrt = parseInt(Math.sqrt(number));
+
+  for (let i = 2; i <= sqrt; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const getReversedPrime = (numArr) => {};
+
+console.log(getReversedPrime([32, 55, 62, 20, 250, 370, 200, 30, 100]));
