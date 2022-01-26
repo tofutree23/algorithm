ef8e59e86ddd240f8ecc5944c22c32242543609e
Jonghyung <tofutree23@gmail.com>
2022-01-26 16:52:08 +0900

    del: day20 - giveup

--- 4-bruteforce.js
+++ 4-bruteforce.js
@@ -47,22 +47,3 @@ const getReversedPrime = (numArr) => {
 };
 
 console.log(getReversedPrime([32, 55, 62, 20, 250, 370, 200, 30, 100]));
-
-/** 3. Mentoring */
-const pairMento = (gradeArr) => {
-  const allPairs = [];
-  const testCnt = gradeArr.length;
-  const gradeCnt = gradeArr[0].length;
-  for (let i = 1; i < testCnt + 1; i++) {
-    for (let j = 1; j < gradeCnt + 1; j++) {
-      allPairs.push([i, j]);
-    }
-  }
-};
-console.log(
-  pairMento([
-    [3, 4, 1, 2],
-    [4, 3, 2, 1],
-    [3, 1, 4, 2],
-  ])
-);
