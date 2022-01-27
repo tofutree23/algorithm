ab60746c72461439bbf81e072b5b9b1d5d395501
Jonghyung <tofutree23@gmail.com>
2022-01-27 20:14:32 +0900

    add: day21

--- 5-twopointerSlidingHash.js
+++ 5-twopointerSlidingHash.js
@@ -19,3 +19,23 @@ const mergeArray = (arr1, arr2) => {
   return mergedArr;
 };
 console.log(mergeArray([1, 3, 5], [2, 3, 6, 7, 9]));
+
+/** 2. Common element */
+const getCommonElement = (elementArr1, elementArr2) => {
+  const commonArr = [];
+  const arr1 = elementArr1.sort();
+  const arr2 = elementArr2.sort();
+
+  let pointerOne = 0;
+  let pointerTwo = 0;
+
+  while (pointerOne < arr1.length && pointerTwo < arr2.length) {
+    if (arr1[pointerOne] == arr2[pointerTwo]) {
+      commonArr.push(arr1[pointerOne++]);
+      pointerTwo++;
+    } else if (arr1[pointerOne] < arr2[pointerTwo]) pointerOne++;
+    else pointerTwo++;
+  }
+  return commonArr;
+};
+console.log(getCommonElement([1, 3, 9, 5, 2], [3, 2, 5, 7, 8]));
