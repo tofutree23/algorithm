021f36a2ce6a9aa588cce8b411f74d3e3b2b5bd9
Jonghyung <tofutree23@gmail.com>
2022-02-06 15:53:18 +0900

    add: day30

--- 6-stackqueue.js
+++ 6-stackqueue.js
@@ -4,12 +4,29 @@ const isCorrentRoundBracket = (string) => {
   for (let x of string) {
     if (x === "(") stack.push(x);
     else {
-      if (stack.length === 0) return "No";
+      if (stack.length === 0) return false;
       stack.pop();
     }
   }
-  if (stack.length > 0) return "No";
+  if (stack.length > 0) return false;
 
-  return "Yes";
+  return true;
 };
 console.log(isCorrentRoundBracket("(()(()))(()"));
+
+/** 2. Remove between () */
+const delBetweenBracket = (string) => {
+  if (isCorrentRoundBracket(string)) return false;
+
+  const stack = [];
+  for (const word of string) {
+    stack.push(word);
+
+    if (word === ")") {
+      while (stack.pop() !== "(");
+    }
+  }
+
+  return stack.join("");
+};
+console.log(delBetweenBracket("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
