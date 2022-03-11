01f5afc5c5e215c72d0e89b912ed5a84fa1ff807
Jonghyung <tofutree23@gmail.com>
2022-03-11 20:19:05 +0900

    add: day31

--- 6-stackqueue.js
+++ 6-stackqueue.js
@@ -30,3 +30,98 @@ const delBetweenBracket = (string) => {
   return stack.join("");
 };
 console.log(delBetweenBracket("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
+
+/** 3. Doll crane */
+const getDolls = (board, moves) => {
+  let exploded = 0;
+  const stack = [];
+
+  moves.forEach((move) => {
+    const position = move - 1;
+    for (let row of board) {
+      if (row[position] !== 0) {
+        const dangled = row[position];
+        row[position] = 0;
+
+        if (dangled === stack[stack.length - 1]) {
+          exploded += 2;
+          stack.pop();
+        } else {
+          stack.push(dangled);
+        }
+        break;
+      }
+    }
+  });
+
+  return exploded;
+};
+console.log(
+  getDolls(
+    [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 3],
+      [0, 2, 5, 0, 1],
+      [4, 2, 4, 4, 2],
+      [3, 5, 1, 3, 1],
+    ],
+    [1, 5, 3, 5, 1, 2, 1, 4]
+  )
+);
+
+/** 4. postfix */
+const calcPostfix = (s) => {
+  const stack = [];
+  for (let x of s) {
+    if (/[0-9]/gi.test(x)) {
+      stack.push(parseInt(x));
+    } else {
+      let answer = 0;
+      const right = stack.pop();
+      const left = stack.pop();
+
+      switch (x) {
+        case "+":
+          answer = left + right;
+          break;
+
+        case "*":
+          answer = left * right;
+          break;
+
+        case "/":
+          answer = left / right;
+          break;
+
+        case "-":
+          answer = left - right;
+        default:
+          break;
+      }
+      stack.push(answer);
+    }
+  }
+  return stack[0];
+};
+console.log(calcPostfix("352+*9-"));
+
+/** 5. How many cutted ironrod */
+const getIronRod = (s) => {
+  const arr = s.split("");
+  const stack = [];
+  let answer = 0;
+  for (let [i, x] of arr.entries()) {
+    if (x === "(") stack.push(x);
+    else {
+      stack.pop();
+      if (arr[i - 1] === "(") {
+        answer += stack.length;
+      } else {
+        answer += 1;
+      }
+    }
+  }
+
+  return answer;
+};
+console.log(getIronRod("()(((()())(())()))(())"));
