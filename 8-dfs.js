d8d41f6e201afd1721c064db4cb439204f2c86b0
Jonghyung <tofutree23@gmail.com>
2022-03-24 00:28:00 +0900

    add: day42

--- 8-dfs.js
+++ 8-dfs.js
@@ -27,3 +27,47 @@ const binaryTreeOrder = (v, preOrder, inOrder, postOrder) => {
   return [preOrder.join(", "), inOrder.join(", "), postOrder.join(", ")];
 };
 console.log(binaryTreeOrder(1, [], [], []));
+
+/** 4. DFS - Subset */
+const subset = (v, check, answer) => {
+  if (v > 3) {
+    const temp = [];
+    for (let i = 0; i < check.length; i++) {
+      if (check[i]) temp.push(i + 1);
+    }
+
+    if (temp.length !== 0) answer.push(temp);
+    return;
+  }
+  check[v - 1] = 1;
+  subset(v + 1, check, answer);
+  check[v - 1] = 0;
+  subset(v + 1, check, answer);
+
+  return answer;
+};
+console.log(subset(1, [0, 0, 0], []));
+
+/** 5. DFS - Subset with the same sum */
+const sameSumSubset = (arr) => {
+  let answer = "NO";
+  let flag = 0;
+  const maxLevel = arr.length;
+  const total = arr.reduce((prev, cur) => prev + cur, 0);
+  (function findSubset(level, sum) {
+    if (flag) return;
+    if (level === maxLevel) {
+      if (total - sum === sum) {
+        answer = "YES";
+        flag = 1;
+      }
+      return;
+    }
+
+    findSubset(level + 1, sum + arr[level]);
+    findSubset(level + 1, sum);
+  })(0, 0);
+
+  return answer;
+};
+console.log(sameSumSubset([1, 3, 5, 6, 7, 10]));
