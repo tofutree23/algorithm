6c36f50ad8513bf8496a22530e3dde0dc0b4ef3b
Luke <tofutree23@gmail.com>
2022-03-31 23:11:56 +0900

    add: day47

--- 9-graph.js
+++ 9-graph.js
@@ -39,3 +39,48 @@ console.log(
     [4, 5],
   ])
 );
+
+/** 2. Path2 - with list */
+const findPath2 = (nodes, link) => {
+  const check = Array(nodes).fill(false);
+  // make list with empty array
+  const graph = Array.from(Array(nodes + 1), () => Array());
+  let paths = 0;
+
+  // push node's number (not index.. that's the reason why array start with index 1)
+  for (let [a, b] of link) {
+    graph[a].push(b);
+  }
+
+  check[1] = true;
+  (function DFS(v) {
+    if (v === nodes) {
+      paths++;
+      return;
+    }
+
+    for (let i = 0; i < graph[v].length; i++) {
+      const node = graph[v][i];
+      if (!check[node]) {
+        check[node] = true;
+        DFS(node);
+        check[node] = false;
+      }
+    }
+  })(1);
+
+  return paths;
+};
+console.log(
+  findPath2(5, [
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [2, 1],
+    [2, 3],
+    [2, 5],
+    [3, 4],
+    [4, 2],
+    [4, 5],
+  ])
+);
