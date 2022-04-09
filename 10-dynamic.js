071856bcfc209e4aaed4893774068e73d51cd5c7
Luke <tofutree23@gmail.com>
2022-04-09 09:24:09 +0900

    add: day53

--- 10-dynamic.js
+++ 10-dynamic.js
@@ -27,3 +27,23 @@ const bridge = (stone) => {
   return dynamic[dynamic.length - 1] + dynamic[dynamic.length - 2];
 };
 console.log(bridge(7));
+
+/** 3. LIS */
+const getLTS = (arr) => {
+  let answer = 0;
+  const dynamic = Array(arr.length).fill(0);
+  for (let i = 0; i < arr.length; i++) {
+    const number = arr[i];
+    let maxLength = 0;
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] < number && dynamic[j] > maxLength) {
+        maxLength = dynamic[j];
+      }
+    }
+    dynamic[i] = maxLength + 1;
+    answer = Math.max(dynamic[i], answer);
+  }
+
+  return answer;
+};
+console.log(getLTS([5, 3, 7, 8, 6, 2, 9, 4]));
